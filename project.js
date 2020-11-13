6a417e767fd056ca9ef64eaf431d6812067db97f
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-13 20:01:19 +0000

    added basic html form and some css and working on grabbing data from the form in js

--- project.js
+++ project.js
@@ -1,4 +1,30 @@
-let message = 'hello';
+const draw = function () {
+  let data = getData();
+  alert(typeof(data))
+}
 
-console.log("hello");
+
+// function to get data from graphForm once submitted
+const getData = function () {
+ const dataPoints = document.querySelector('#dataPoints').value;
+ return parseData(dataPoints)
+};
+
+// parse data into array - array up numbers up to value
+const parseData = function (data) {
+  let parsedData = [];
+  for (let i = 1; i <= data; i++) {
+    parsedData.push(i);
+  }
+  return parsedData
+}
+
+
+
+
+// listen for form submit
+document.getElementById('graphForm').addEventListener('submit', draw);
+
+
+
 
