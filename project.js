f52248982c12644c05def7bbc2bdf42e0b397fb7
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-13 20:56:34 +0000

    implementation of visualization of very basic form data

--- project.js
+++ project.js
@@ -1,8 +1,32 @@
-const draw = function () {
-  let data = getData();
-  alert(typeof(data))
+// drawBarChart function
+
+const drawBarChart = function (data) {
+  // if there is already a graph present on the page, remove it
+  if (document.querySelector('#graphGrid')) {
+    document.querySelector('#graphGrid').remove();
+  }
+  // access where we will put out graphGrid 
+  let graphContainer = document.querySelector('#graphElement');
+  // create graph grid and add to page
+  let graphGrid = document.createElement('div');
+  graphGrid.setAttribute('id', 'graphGrid');
+  graphGrid.classList.add('graphGrid');
+  graphContainer.append(graphGrid);
+  graphGrid = document.querySelector('#graphGrid');
+  
+  for (let i = 0; i < data.length; i++) {
+    let bar = document.createElement('div');
+    bar.classList.add('bar')
+    graphGrid.append(bar);
+  }
 }
 
+
+const draw = function () {
+  let data = getData();
+  
+  drawBarChart(data);
+}
 
 // function to get data from graphForm once submitted
 const getData = function () {
@@ -18,9 +42,6 @@ const parseData = function (data) {
   }
   return parsedData
 }
-
-
-
 
 // listen for form submit
 document.getElementById('graphForm').addEventListener('submit', draw);
